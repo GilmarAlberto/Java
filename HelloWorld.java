9f509ad5b078e9d7fca86ce5564d57f13621fce9
Gilmar Alberto de Abreu Pinto <gilabreu00@gmail.com>
2022-11-21 14:58:42 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Progrma que imprime em tela Olá Mundo!
+ * seguido de outra linha
+ * @author Gilmar Alberto
+ */
+
+public class HelloWorld {
+
+    /* Método principal da classe */
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!"); 
+        System.out.println("Meu primeiro programa em Java!");
+    } //fim do método
+} // fim da classe
