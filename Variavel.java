fd1b4da87ce17c12e1bc8f5cde52113e7bd6317a
Gilmar Alberto de Abreu Pinto <gilabreu00@gmail.com>
2022-11-22 05:40:27 -0300

    Versão 1.0 programa Variavel.java curso java básico

---
+++ Variavel.java
@@ -0,0 +1,13 @@
+/**
+ * Conceitos de variável
+ *
+ * @author Gilmar Alberto
+ */
+
+public class Variavel {
+
+    /* Método principal da classe */
+    public static void main(String[] args) {
+        System.out.println("Texto Simples!");
+    } //fim do método
+} // fim da classe
