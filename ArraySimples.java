effdf77c04d13a2f9af824162c0e1d27d153a06e
Gilmar Alberto <gilabreu00@gmail.com>
2022-11-22 14:48:48 -0300

    Teste array

---
+++ ArraySimples.java
@@ -0,0 +1,19 @@
+/**
+ * Conceitos de Arrays
+ *
+ * @author Gilmar Alberto
+ */
+
+import java.util.Arrays;
+
+public class ArraySimples {
+
+    /* Método principal da classe */
+    public static void main(String[] args) {
+        String[] paises = {"BRASIL", "RUSSIA", "INDIA", "CHINA"};
+
+        System.out.println(paises[0]);
+        System.out.println(Arrays.toString(paises));
+        
+    } //fim do método
+} // fim da classe
