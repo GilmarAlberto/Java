918f18c3167353412b74c5d487228e9eed0553f0
Gilmar Alberto <gilabreu00@gmail.com>
2022-11-27 09:58:44 -0300

    Exercicios Desafio004
    input

---
+++ Desafio004.java
@@ -0,0 +1,17 @@
+/**
+ * Programa que imprime em tela Olá Mundo!
+ * seguido de outra linha
+ * @author Gilmar Alberto
+ */
+
+public class Desafio004 {
+
+    /* Método principal da classe */
+    public static void main(String[] args) {
+
+        System.out.println("Digite alguma coisa: ");
+        String entrada = System.console().readLine();
+
+        System.out.println("Você digitou: " + entrada);
+    } //fim do método
+} // fim da classe
